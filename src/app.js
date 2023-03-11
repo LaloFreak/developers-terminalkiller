1271d653237c3a1b5ae20d833a8925455f80e85c
Lalo Freak <lalo.gwerh@gmail.com>
2023-03-11 01:49:08 -0300

    thrash

--- src/app.js
+++ src/app.js
@@ -1,25 +1,19 @@
-require("dotenv").config();
-const morgan = require('morgan');
-const express = require('express');
-const server = express();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-const routes = require('./routes/index.js');
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
 
-server.use((req, res, next)=>{
-    console.log('request from: ', req.headers.origin)
-    console.log('method: ', req.method)
-    console.log('route: ', req.url)
-    res.header('Access-Control-Allow-Origin', "*");
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested, Content-Type, Accept');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
+app.get("/", (req, res) => {
+  res.send("¡Hola, mundo!");
 });
 
-server.use(express.json())
-server.use(express.urlencoded({extended: true}));
-server.use(morgan('dev'));
-server.use(express.json());
-server.use('/', routes);
+app.get("/users/:id", (req, res) => {
+  const id = req.params.id;
+  // ... Lógica para obtener usuario con ID "id" ...
+  res.send(user);
+});
 
-module.exports = server
+module.exports = app;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,4 @@
-const express = require('express');
-const app = express();
+const functions = require("firebase-functions");
+const app = require("./app");
 
-// Define una ruta de ejemplo para tu API
-app.get('/api/example', (req, res) => {
-  res.send('¡Hola desde tu API de Node.js en Firebase Hosting!');
-});
-
-// Exporta tu app de Express como una función
 exports.yourExpressApp = functions.https.onRequest(app);
