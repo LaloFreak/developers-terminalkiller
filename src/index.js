2ceac037ea74132aeda34be6ff150bd477e04464
Lalo Freak <lalo.gwerh@gmail.com>
2023-03-11 00:43:07 -0300

    thrash

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
+const port = process.env.PORT || 8080
+const server = require('./app');
 
+async function main() {
+    server.listen(port, () => console.log(`Servidor escuchando en el puerto ${port}`));
+}
+exports.developers = main();
 
