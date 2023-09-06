64433f233b8cc8871cb1c8cde2100f9957f4a484
Lalo Freak <lalofreak.dev@gmail.com>
2023-09-06 12:09:30 -0300

    adding design-api

--- src/controllers/api-design.js
+++ src/controllers/api-design.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
-/* const design = require('../../developers/Gwerh/design-api.json');
- */
+const design = require('../../developers/Gwerh/design-api.json');
+
 
 router.get("/", async (req, res) => {
-  res.status(200).send('design')
+  res.status(200).send(design)
 });
 
 module.exports = router;
