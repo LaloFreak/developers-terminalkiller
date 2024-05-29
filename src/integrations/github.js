62f341f6beaad87341aa75571733aae2bf76db32
Lalo Freak <lalo.gwerh@gmail.com>
2024-05-29 17:27:54 -0400

    adding try catch to github integration

--- src/integrations/github.js
+++ src/integrations/github.js
@@ -1,13 +1,18 @@
 const axios = require('axios');
 
 const getGithubRepos = async(username, token) => {
-  const repositories = await axios.get(`https://api.github.com/users/${username}/repos`, {
-    headers: {
-      Authorization: `token ${token}`,
-    }
-  });
-
-  return repositories;
+  try {
+    const repositories = await axios.get(`https://api.github.com/users/${username}/repos`, {
+      headers: {
+        Authorization: `token ${token}`,
+      }
+    });
+  
+    return repositories;
+    
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 module.exports = {
