44b5c4a1523ef8920aa58e5e5ce6317e42f3e0fb
GwerhDev <gwerh.dev@gmail.com>
2024-06-21 10:01:43 -0400

    adding develop featured api

--- src/controllers/api-develop.js
+++ src/controllers/api-develop.js
@@ -1,8 +1,13 @@
 const router = require('express').Router();
 const develop = require('../../developers/Gwerh/develop-api.json');
+const featured = require('../../developers/Gwerh/develop-featured.json');
 
 router.get("/", async (req, res) => {
   res.status(200).send(develop)
+});
+
+router.get("/featured", async (req, res) => {
+  res.status(200).send(featured)
 });
 
 module.exports = router;
