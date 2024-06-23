562cc0abee3b8dfba67a99f5a637ff2bf250fd6a
GwerhDev <gwerh.dev@gmail.com>
2024-06-22 22:15:54 -0400

    deleting some shitty comments

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ async function main() {
     try {
       await db.connect();
       console.log("succesfully connected");
-      server.listen(PORT, ()=> console.log(`server listening on port ${PORT}`))//MODO PRUEBAS
+      server.listen(PORT, ()=> console.log(`server listening on port ${PORT}`))
     } catch (error) {
-      server.listen(PORT, ()=> console.log(`server listening on port ${PORT} (db disconected)`))//MODO PRUEBAS
+      server.listen(PORT, ()=> console.log(`server listening on port ${PORT} (db disconected)`))
       console.error("Unable to connect to database");
     }
 }
