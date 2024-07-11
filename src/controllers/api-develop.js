2717ebec91d3e94ba36c088f0893ed0aca876b86
Gwerh <gwerh.dev@gmail.com>
2024-07-11 16:07:38 -0400

    updating develop api, creating 'featured' boolean fieldd & deleting featured develop api

--- src/controllers/api-develop.js
+++ src/controllers/api-develop.js
@@ -1,13 +1,13 @@
 const router = require('express').Router();
 const develop = require('../../developers/Gwerh/develop-api.json');
-const featured = require('../../developers/Gwerh/develop-featured.json');
 
 router.get("/", async (req, res) => {
-  res.status(200).send(develop)
+  return res.status(200).send(develop);
 });
 
 router.get("/featured", async (req, res) => {
-  res.status(200).send(featured)
+  const featured = develop[0]?.web.filter((e) => e.featured === true);
+  return res.status(200).send(featured);
 });
 
 module.exports = router;
