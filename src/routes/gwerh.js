e1afe23cd58d9dca799fa9f801bd9d9f0c4e8d14
Gwerh <gwerh.dev@gmail.com>
2024-07-15 15:31:30 -0400

    adding experience api

--- src/routes/gwerh.js
+++ src/routes/gwerh.js
@@ -5,11 +5,13 @@ const packages = require('../controllers/packages');
 const apiSound = require('../controllers/api-sound');
 const apiDesign = require('../controllers/api-design');
 const apiDevelop = require('../controllers/api-develop');
+const apiExperience = require('../controllers/api-experience');
 
 router.use("/github", github);
 router.use("/getsound", apiSound);
 router.use("/packages", packages);
 router.use("/getdesign", apiDesign);
 router.use("/getdevelop", apiDevelop);
+router.use("/getexperience", apiExperience);
 
 module.exports = router;
